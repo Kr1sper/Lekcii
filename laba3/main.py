6df088ee8c99218db035c62013030de4fd5f217e
Kr1sper <kovbasa_2003@bk.ru>
2023-03-18 11:09:22 +0300

    Выполнил задание

--- laba3/main.py
+++ laba3/main.py
@@ -1,8 +1,22 @@
-op=open("1.txt", "r")
-wr=open("2.txt", "w")
+import os.path
 
-A=op.read()
-wr.write(A)
+flag = 'false'
+if os.path.isfile("data/app.conf"):
+    with open("data/app.conf", "r") as f:
+        for line in f:
+            a, b, c = line.split()
 
-op.close()
-wr.close()
+else:
+    with open("data/app.conf", "w+") as g:
+        g.write(input())
+    with open("data/app.conf", "r") as e:
+        for line in e:
+            a, b, c = line.split()
+
+my_arr=[] * int(a)
+for i in range(0, int(a)):
+    d = int(b)*i+int(c)
+    my_arr.insert(i, d)
+
+with open("data/result.dat","w+") as v:
+    v.write(' '.join(map(str,my_arr)))
